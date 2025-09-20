dce43d11002eceb0888f63181df91a7527bad3eb
Anqi <anqiyang1270@alumnes.ilerna.com>
2025-09-20 13:20:16 +0200

    Laboratorio 2 - Ejercicio 2 completado

---
+++ laboratorio 1/ejercicio 2/script.js
@@ -0,0 +1,24 @@
+var numA = 13;
+var numB = 17;
+
+/*suma*/
+var rS = numA + numB;
+console.log("The result is :" + rS);
+
+/*meno*/
+var rM = numB - numA;
+console.log("The result is :" + rM);
+
+/*multiplicar*/
+var rX = numA * numB;
+console.log("The result is :" + rX);
+
+
+/*divitir*/
+var rD = numA / numB;
+console.log("The result is :" + rD);
+
+/*Resto*/
+var rR = numB %2;
+console.log("El resto es " + rR);
+
