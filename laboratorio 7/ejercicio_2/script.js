111da29460ac96f64bc2531ff5a33d85ab7b08bf
Anqi <anqiyang1270@alumnes.ilerna.com>
2025-12-02 20:08:11 +0100

    Ejercicio 2 completo

---
+++ laboratorio 7/ejercicio_2/script.js
@@ -0,0 +1,11 @@
+function MValor(){
+    console.log("Ejercicio 2")
+
+    let nombre = document.getElementById("nombre").value;
+    let fecha = document.getElementById("fecha").value;
+    let pais = document.getElementById("pais").value;
+
+    console.log("Nombre:", nombre);
+    console.log("Fecha de nacimiento:", fecha);
+    console.log("País:", pais);
+}
